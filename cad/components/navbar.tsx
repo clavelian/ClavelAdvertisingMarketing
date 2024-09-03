0492d86a86640b5cf4f74970cbd28c1bcae56f7a
Ian Clavel <ianclavel1991@gmail.com>
2024-09-04 03:16:54 +0800

    feat: added flipwords

--- cad/components/navbar.tsx
+++ cad/components/navbar.tsx
@@ -7,11 +7,12 @@ import {
   ProductItem,
 } from "../components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
+import { ModeToggle } from "../components/ui/lnd";
 
 export function NavbarDemo() {
   return (
     <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2" />
+      <Navbar className="top-10" />
       <p className="text-black dark:text-white"> </p>
     </div>
   );
@@ -21,7 +22,7 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-0 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Services">
@@ -50,6 +51,9 @@ function Navbar({ className }: { className?: string }) {
             <HoveredLink href="/enterprise">Enterprise</HoveredLink>
           </div>
         </MenuItem>
+        <span className="">
+          <ModeToggle />
+        </span>
       </Menu>
     </div>
   );
