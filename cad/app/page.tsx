f101cbcb281f09c3fea824a62763dec56b3e3bf5
Ian Clavel <ianclavel1991@gmail.com>
2024-09-05 05:02:10 +0800

    minor changes

--- cad/app/page.tsx
+++ cad/app/page.tsx
@@ -1,11 +1,15 @@
 import { NavigationMenuDemo } from "../components/navmenu";
 import FlipWordsDemo from "../components/flipwords";
+import { ModeToggle } from "../components/ui/lnd";
 
 export default function Home() {
   return (
     <>
-      <nav>
-        <NavigationMenuDemo />{" "}
+      <nav className="pt-6 flex items-center justify-center gap-3">
+        <NavigationMenuDemo />
+        <span>
+          <ModeToggle />
+        </span>
       </nav>
       <main>
         <FlipWordsDemo />
