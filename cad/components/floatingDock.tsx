574d5297f9eff5b5891c0b82bd324838f0464b4c
Ian Clavel <ianclavel1991@gmail.com>
2024-09-08 02:32:20 +0800

    feat: floating dock

--- cad/components/floatingDock.tsx
+++ cad/components/floatingDock.tsx
@@ -36,11 +36,11 @@ export function FloatingDockDemo() {
       ),
       href: "#",
     },
-    {
-      title: "Aceternity UI",
-      icon: <Image src="" width={20} height={20} alt="Aceternity Logo" />,
-      href: "#",
-    },
+    // {
+    //   title: "Aceternity UI",
+    //   icon: <Image src="" width={20} height={20} alt="Aceternity Logo" />,
+    //   href: "#",
+    // },
     {
       title: "Changelog",
       icon: (
@@ -65,11 +65,8 @@ export function FloatingDockDemo() {
     },
   ];
   return (
-    <div className="flex items-center justify-center h-[35rem] w-full">
-      <FloatingDock
-        mobileClassName="translate-y-20" // only for demo, remove for production
-        items={links}
-      />
+    <div className="">
+      <FloatingDock items={links} />
     </div>
   );
 }
