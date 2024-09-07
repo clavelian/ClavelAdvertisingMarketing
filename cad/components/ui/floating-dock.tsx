af31d1f6632147bb7d0d7d5a89dd30ebee5a97ec
Ian Clavel <ianclavel1991@gmail.com>
2024-09-08 02:32:20 +0800

    feat: floating dock

--- cad/components/ui/floating-dock.tsx
+++ cad/components/ui/floating-dock.tsx
@@ -70,9 +70,9 @@ const FloatingDockMobile = ({
                 <Link
                   href={item.href}
                   key={item.title}
-                  className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
+                  className="h-[2.3rem] w-[2.3rem] rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
                 >
-                  <div className="h-4 w-4">{item.icon}</div>
+                  <div className="h-10 w-10">{item.icon}</div>
                 </Link>
               </motion.div>
             ))}
@@ -81,9 +81,9 @@ const FloatingDockMobile = ({
       </AnimatePresence>
       <button
         onClick={() => setOpen(!open)}
-        className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-800 flex items-center justify-center"
+        className="h-[2.3rem] w-[2.3rem] rounded-full bg-gray-50 dark:bg-neutral-800 flex items-center justify-center"
       >
-        <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+        <IconLayoutNavbarCollapse className="h-10 w-10 text-neutral-500 dark:text-neutral-400" />
       </button>
     </div>
   );
