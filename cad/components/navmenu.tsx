537d767dfbf1ca82926ffb70e46bc3a583e30b74
Ian Clavel <ianclavel1991@gmail.com>
2024-09-13 02:28:44 +0800

    ffs

--- cad/components/navmenu.tsx
+++ cad/components/navmenu.tsx
@@ -59,17 +59,9 @@ export function NavigationMenuDemo() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>Get started</NavigationMenuTrigger>
           <NavigationMenuContent>
-<<<<<<< Updated upstream
             <ul className="grid p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <ListItem href="/docs" title="About Clavel Marketing">
                 Learn more about us.
-=======
-            <ul className="grid gap-3 p-4 md:w-[300px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              {/* <NavigationMenuLink asChild></NavigationMenuLink> */}
-
-              <ListItem href="/docs" title="Home">
-                Re-usable components built using Radix UI and Tailwind CSS.
->>>>>>> Stashed changes
               </ListItem>
               <ListItem href="/docs/installation" title="Services">
                 Explore our services, from SEO to Social Media Management etc.
