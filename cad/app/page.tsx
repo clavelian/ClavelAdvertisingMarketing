915750ee78856c948acf8cbf4d89e0f53c6cf0d9
Ian Clavel <ianclavel1991@gmail.com>
2024-09-14 05:15:10 +0800

    feat: added and edited hero

--- cad/app/page.tsx
+++ cad/app/page.tsx
@@ -6,6 +6,7 @@ import { NavigationMenuDemo } from "../components/navmenu";
 import { ModeToggle } from "../components/ui/lnd";
 // import { FloatingDockDemo } from "../components/floatingDock";
 import { StickyScrollRevealDemo } from "../components/stickyScroll";
+import { HeroHighlightDemo } from "../components/hero";
 
 export default function Home() {
   return (
@@ -37,16 +38,19 @@ export default function Home() {
         <span className="exclude:pl-2 mobile:absolute mobile:right-10 mobile:top-14">
           <ModeToggle />
         </span>
-
-        {/* =================body section================= */}
       </nav>
-      <main className="">{/* <FlipWordsDemo /> */}</main>
+      {/* =================body section================= */}
+      <main className="">
+        {/* <FlipWordsDemo /> */}
+        <HeroHighlightDemo />
+      </main>
       <div className="fixed bottom-[10rem] right-[6rem]">
         <aside className="">{/* <FloatingDockDemo /> */}</aside>
       </div>
       <div className=" flex items-center justify-center">
         <StickyScrollRevealDemo />
       </div>
+      <div></div>
     </>
   );
 }
