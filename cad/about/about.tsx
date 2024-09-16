ed1f0077ae29c0ebc6f5988673f1fb1637797b0f
Ian Clavel <ianclavel1991@gmail.com>
2024-09-16 17:10:19 +0800

    creating aboutme page

---
+++ cad/about/about.tsx
@@ -0,0 +1,13 @@
+import { NavigationMenuDemo } from "../components/navmenu";
+
+const Aboutme = () => {
+  return (
+    <>
+      <nav>
+        <NavigationMenuDemo />
+      </nav>
+    </>
+  );
+};
+
+export default Aboutme;
--- cad/pages/about.tsx
+++
@@ -1,5 +0,0 @@
-const About = () => {
-  return <div>This page is about about me.</div>;
-};
-
-export default About;
