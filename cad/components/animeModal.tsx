c1208bdb4fcaa2dccc431457ab7ed5b4728fcfa6
Ian Clavel <ianclavel1991@gmail.com>
2024-09-18 15:49:59 +0800

    idk

--- cad/components/animeModal.tsx
+++ cad/components/animeModal.tsx
@@ -9,14 +9,15 @@ import {
 } from "../components/ui/animated-modal";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import c1 from "@/public/c1.png";
-import c2 from "@/public/c2.png";
-import c3 from "@/public/c3.png";
-import c4 from "@/public/c4.png";
-import c5 from "@/public/c5.png";
 
 export function AnimatedModalDemo() {
-  const images = [{ c1, alt: "christmas" }];
+  const images = [
+    "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
   return (
     <div className="py-40  flex items-center justify-center">
       <Modal>
