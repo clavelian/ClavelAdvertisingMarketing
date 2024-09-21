98aae2226597366e6b692edbd36d6f930376ea62
Ian Clavel <ianclavel1991@gmail.com>
2024-09-21 23:50:23 +0800

    added pictures for samples

--- cad/components/sampleTab.tsx
+++ cad/components/sampleTab.tsx
@@ -2,21 +2,36 @@
 
 import Image from "next/image";
 import { Tabs } from "@/components/ui/tabs";
+import Instagram1 from "@/public/insta1.webp";
+import Instagram2 from "@/public/insta2.webp";
 
 export function TabsDemo() {
   const tabs = [
     {
-      title: "Product",
+      title: "Instagram Post",
       value: "product",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-purple-900 to-violet-900">
+        <div className="grid w-full overflow-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-purple-900 to-violet-900">
           <p className="text-xl md:text-4xl ">Product Tab</p>
-          <DummyContent />
+          <Image
+            src={Instagram1}
+            alt="Instagram post wave"
+            height={100}
+            width={100}
+          />
+          <Image
+            src={Instagram2}
+            alt="idk tal;agashet"
+            height={100}
+            width={100}
+          />
+
+          {/* <DummyContent /> */}
         </div>
       ),
     },
     {
-      title: "Services",
+      title: "GIF Post",
       value: "services",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
@@ -26,7 +41,7 @@ export function TabsDemo() {
       ),
     },
     {
-      title: "Playground",
+      title: "Brand Board",
       value: "playground",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
@@ -36,7 +51,7 @@ export function TabsDemo() {
       ),
     },
     {
-      title: "Content",
+      title: "Logo",
       value: "content",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
@@ -56,12 +71,13 @@ export function TabsDemo() {
 
 const DummyContent = () => {
   return (
-    <Image
-      src=""
-      alt="dummy image"
-      width="1000"
-      height="1000"
-      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
-    />
+    // <Image
+    //   src=""
+    //   alt="dummy image"
+    //   width="1000"
+    //   height="1000"
+    //   className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+    // />
+    <div></div>
   );
 };
