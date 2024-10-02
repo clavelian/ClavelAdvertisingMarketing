c0bcaf85d1e0338241f965ed7e5b469634f68a9d
Ian Clavel <ianclavel1991@gmail.com>
2024-10-02 20:18:06 +0800

    all lines commented for floatingdocks

--- cad/components/example/floatingDock.tsx
+++ cad/components/example/floatingDock.tsx
@@ -1,71 +1,71 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { FloatingDock } from "@/components/ui/floating-dock";
-import {
-  IconBrandLinkedin,
-  IconBrandFacebook,
-  IconBrandX,
-  IconHome,
-  IconBrandInstagram,
-  IconMail,
-} from "@tabler/icons-react";
-import Image from "next/image";
+// import React from "react";
+// import { FloatingDock } from "@/components/ui/floating-dock";
+// import {
+//   IconBrandLinkedin,
+//   IconBrandFacebook,
+//   IconBrandX,
+//   IconHome,
+//   IconBrandInstagram,
+//   IconMail,
+// } from "@tabler/icons-react";
+// import Image from "next/image";
 
-export function FloatingDockDemo() {
-  const links = [
-    {
-      title: "Home",
-      icon: (
-        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    // {
-    //   title: "Aceternity UI",
-    //   icon: <Image src="" width={20} height={20} alt="Aceternity Logo" />,
-    //   href: "#",
-    // },
+// export function FloatingDockDemo() {
+//   const links = [
+//     {
+//       title: "Home",
+//       icon: (
+//         <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "#",
+//     },
+//     // {
+//     //   title: "Aceternity UI",
+//     //   icon: <Image src="" width={20} height={20} alt="Aceternity Logo" />,
+//     //   href: "#",
+//     // },
 
-    {
-      title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "FB",
-      icon: (
-        <IconBrandFacebook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "https://www.facebook.com/Rosas04",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "IconMail",
-      icon: (
-        <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "IconBrandInstagram",
-      icon: (
-        <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-  ];
-  return (
-    <div className="">
-      <FloatingDock items={links} />
-    </div>
-  );
-}
+//     {
+//       title: "Twitter",
+//       icon: (
+//         <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "#",
+//     },
+//     {
+//       title: "FB",
+//       icon: (
+//         <IconBrandFacebook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "https://www.facebook.com/Rosas04",
+//     },
+//     {
+//       title: "GitHub",
+//       icon: (
+//         <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "#",
+//     },
+//     {
+//       title: "IconMail",
+//       icon: (
+//         <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "#",
+//     },
+//     {
+//       title: "IconBrandInstagram",
+//       icon: (
+//         <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+//       ),
+//       href: "#",
+//     },
+//   ];
+//   return (
+//     <div className="">
+//       <FloatingDock items={links} />
+//     </div>
+//   );
+// }
