3fd7251ed4d179062b0e710e83fadef923a6b78e
Ian Clavel <ianclavel1991@gmail.com>
2024-10-03 01:51:47 +0800

    feat: minor adjustment in the navbar, further improvement in the footer

--- cad/components/footer.tsx
+++ cad/components/footer.tsx
@@ -1,8 +1,20 @@
+import {
+  IconBrandLinkedin,
+  IconBrandFacebook,
+  IconBrandX,
+  IconHome,
+  IconBrandInstagram,
+  IconMail,
+} from "@tabler/icons-react";
+
 const Footer = () => {
   return (
     <>
       <div className="w-full h-[6rem] bottom-0 bg-gray-700 dark:bg-gray-900 text-white flex items-center justify-center">
         <div className="flex justify-center items-center">zxczxc</div>
+        <div className="bg-transparent rounded-md hover:bg-blue-600 transition ease-in-out delay-150 w-[2rem] h-[2rem] justify-center items-center flex">
+          <IconBrandFacebook />
+        </div>
       </div>
     </>
   );
