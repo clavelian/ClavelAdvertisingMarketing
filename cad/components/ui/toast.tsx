46a1a4f22c4ea4eb45c7559cfdf6d8999a9086fb
Ian Clavel <ianclavel1991@gmail.com>
2024-10-05 23:56:34 +0800

    feat: relocated toaster to lower right

--- cad/components/ui/toast.tsx
+++ cad/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-[18rem] flex-col-reverse p-4 mobile:bottom-0 mobile:right-10 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 z-[100] flex max-h-screen w-[18rem] flex-col-reverse p-4 mobile:bottom-0 mobile:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
