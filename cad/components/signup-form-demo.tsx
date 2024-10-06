1b24fbd52d4d42bc13d629f3bc6f62b140ae6b76
Ian Clavel <ianclavel1991@gmail.com>
2024-10-06 16:10:52 +0800

    feat: integrated toast to sign up button

--- cad/components/signup-form-demo.tsx
+++ cad/components/signup-form-demo.tsx
@@ -3,13 +3,10 @@ import React from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import { useToast } from "@/hooks/use-toast";
 
 export default function SignupFormDemo() {
+  const { toast } = useToast();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -54,6 +51,11 @@ export default function SignupFormDemo() {
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
+          onClick={() => {
+            toast({
+              description: "Your message has been sent.",
+            });
+          }}
         >
           Sign up &rarr;
           <BottomGradient />
