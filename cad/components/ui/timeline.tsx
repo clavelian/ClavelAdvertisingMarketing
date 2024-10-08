c0209603b7883ff98c40f770cea03a9c7674e572
Ian Clavel <ianclavel1991@gmail.com>
2024-10-08 23:39:20 +0800

    feat: edited timeline.tsx but might swap the design with aboutme page

--- cad/components/ui/timeline.tsx
+++ cad/components/ui/timeline.tsx
@@ -59,7 +59,8 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 exclude:left-3 w-10 rounded-full bg-white dark:bg-slate-300 flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden exclude:block text-xl exclude:pl-20 exclude:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              {/* circle font */}
+              <h3 className="hidden exclude:block text-xl exclude:pl-20 exclude:text-3xl font-bold text-neutral-500 dark:text-neutral-500 ">
                 {item.title}
               </h3>
             </div>
