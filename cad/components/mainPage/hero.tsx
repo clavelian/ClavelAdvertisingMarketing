6c6a8650222c22959b82793ae285bfd2728adb25
Ian Clavel <ianclavel1991@gmail.com>
2024-10-11 12:22:48 +0800

    feat: centered items

--- cad/components/mainPage/hero.tsx
+++ cad/components/mainPage/hero.tsx
@@ -4,14 +4,18 @@ import { Highlight } from "../ui/hero-highlight";
 export function HeroHighlight() {
   return (
     <>
-      <div>
-        From{" "}
-        <Highlight className="text-black dark:text-white">
+      <div className="pb-4 justify-center items-center flex">
+        <span className="text-4xl">From</span>{" "}
+        <Highlight className="text-black dark:text-white text-6xl font-semibold">
           Chaos to Clarity <br />
         </Highlight>
       </div>
-      Clavel Advertising Marketing’s and VA Services <br />
-      <span className="text-base font-medium">
+      <span className="text-4xl">
+        {" "}
+        Clavel Advertising Marketing’s and VA Services{" "}
+      </span>
+      <br />
+      <span className="text-xl font-medium justify-center items-center flex pt-4">
         Focus on what matters with seamless support.
       </span>
     </>
