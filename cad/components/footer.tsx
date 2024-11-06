4acc7959d87fcc5b8ceb96cbdfa3724623b25093
Ian Clavel <ianclavel1991@gmail.com>
2024-11-06 11:02:44 +0800

    footer.tsx -> deleted unused import icons, fixed: logo don't show colored backgrounds in light mode

--- cad/components/footer.tsx
+++ cad/components/footer.tsx
@@ -1,10 +1,5 @@
 import {
-  IconBrandLinkedin,
   IconBrandFacebook,
-  IconBrandX,
-  IconHome,
-  IconBrandInstagram,
-  IconMail,
   IconBrandTiktok,
   IconBrandTwitter,
 } from "@tabler/icons-react";
@@ -25,13 +20,13 @@ const Footer = () => {
         </Link>
 
         <Link href="#">
-          <div className="rounded-md dark:hover:bg-slate-600 bg-black dark:bg-transparent hover:-translate-y-2 dark:transition hover:scale-125  transition ease-in-out delay-150 w-[2rem] h-[2rem] justify-center items-center flex">
+          <div className="rounded-md dark:hover:bg-slate-600 dark:bg-transparent hover:-translate-y-2 dark:transition hover:scale-125  transition ease-in-out delay-150 w-[2rem] h-[2rem] justify-center items-center flex bg-slate-600">
             <IconBrandTiktok />
           </div>
         </Link>
 
         <Link href="#">
-          <div className="bg-transparent rounded-md dark:hover:bg-blue-400 bg-blue-400 dark:bg-transparent hover:-translate-y-2 dark:transition hover:scale-125  transition ease-in-out delay-150 w-[2rem] h-[2rem] justify-center items-center flex">
+          <div className="rounded-md dark:hover:bg-slate-600 dark:bg-transparent hover:-translate-y-2 dark:transition hover:scale-125  transition ease-in-out delay-150 w-[2rem] h-[2rem] justify-center items-center flex bg-blue-400">
             <IconBrandTwitter />
           </div>
         </Link>
